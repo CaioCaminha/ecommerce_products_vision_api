1897e26967a5d9a1ceae165a1ffbedce231042bc
Caio Caminha <caminhacaiopro@gmail.com>
2023-02-14 06:42:45 -0300

    initial commit

---
+++ src/main/kotlin/com/caio/caminha/EcommerceProductsVisionAPI/EcommerceProductsVisionApiApplication.kt
@@ -0,0 +1,11 @@
+package com.caio.caminha.EcommerceProductsVisionAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceProductsVisionApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcommerceProductsVisionApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/caio/caminha/EcommerceProductsVisionAPI/EcommerceProductsVisionApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.caio.caminha.EcommerceProductsVisionAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceProductsVisionApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
